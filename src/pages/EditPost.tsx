2603682d1614a68ac9834f678bc195db666f4858
B24DCCC035nguyenlegiabao <kazokuzox@gmail.com>
2025-10-23 09:02:57 +0700

    Update EditPost.tsx

--- src/pages/EditPost.tsx
+++ src/pages/EditPost.tsx
@@ -1,35 +1,13 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import PostForm from "../components/PostForm";
-import { Post } from "../types/Post";
 
-interface EditPostProps {
-  posts: Post[];
-  setPosts: React.Dispatch<React.SetStateAction<Post[]>>;
-}
-
-const EditPost = ({ posts, setPosts }: EditPostProps) => {
-  const { id } = useParams<{ id: string }>();
-  const navigate = useNavigate();
-  const postToEdit = posts.find((p) => p.id === id);
-
-  const handleEdit = (data: Post) => {
-    setPosts((prevPosts) =>
-      prevPosts.map((post) => (post.id === id ? data : post))
-    );
-    alert("Cập nhật thành công!");
-    navigate(`/posts/${id}`);
-  };
-
-  if (!postToEdit) {
-    return <div>Không tìm thấy bài viết.</div>;
-  }
+export default function EditPost() {
+  const { id } = useParams();
 
   return (
-    <div>
-      <h1>Chỉnh sửa bài viết</h1>
-      <PostForm mode="edit" initialData={postToEdit} onSubmit={handleEdit} />
+    <div style={{ padding: "20px" }}>
+      <h2>Chỉnh sửa bài viết #{id}</h2>
+      <PostForm />
     </div>
   );
-};
-
-export default EditPost;
+}

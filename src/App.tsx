a4d3543dbd5b7d1426ea81020dfe0015a7a1242a
B24DCCC035nguyenlegiabao <kazokuzox@gmail.com>
2025-10-23 09:18:39 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,19 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Post } from "./types/Post";
-import { initialPosts } from "./data/mockPosts";
-import MainLayout from "./layouts/MainLayout";
+import Navbar from "./components/Navbar";
 import PostList from "./pages/PostList";
 import PostDetail from "./pages/PostDetail";
-import CreatePost from "./pages/CreatePost";
+import PostForm from "./components/PostForm";
 import EditPost from "./pages/EditPost";
-import "./App.css"; // Sẽ thêm CSS ở file index.css
 
 function App() {
-  // Quản lý state tập trung tại App
-  const [posts, setPosts] = useState<Post[]>(initialPosts);
-
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<MainLayout />}>
-          {/* Trang chủ */}
-          <Route
-            index
-            element={<PostList posts={posts} setPosts={setPosts} />}
-          />
-          <Route
-            path="/posts"
-            element={<PostList posts={posts} setPosts={setPosts} />}
-          />
-
-          {/* Trang Tạo bài */}
-          <Route path="/create" element={<CreatePost setPosts={setPosts} />} />
-          <Route
-            path="/posts/create"
-            element={<CreatePost setPosts={setPosts} />}
-          />
-
-          {/* Trang Chi tiết */}
-          <Route
-            path="/posts/:id"
-            element={<PostDetail posts={posts} setPosts={setPosts} />}
-          />
-
-          {/* Trang Chỉnh sửa */}
-          <Route
-            path="/posts/edit/:id"
-            element={<EditPost posts={posts} setPosts={setPosts} />}
-          />
-        </Route>
+        <Route path="/" element={<PostList />} />
+        <Route path="/create" element={<PostForm />} />
+        <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/posts/edit/:id" element={<EditPost />} />
       </Routes>
     </BrowserRouter>
   );
